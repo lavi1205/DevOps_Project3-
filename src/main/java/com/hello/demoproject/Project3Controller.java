3a42fc8b230c5d3fc82f35fb1a038bd64f88ca68
lavi1205 <thinhphamthe874@gmail.com>
2023-04-03 01:54:16 +0700

    Update file

--- src/main/java/com/hello/demoproject/Project3Controller.java
+++ src/main/java/com/hello/demoproject/Project3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Project3Controller {
     @RequestMapping(value = "/")
     public String index(){
-        return "test";
+        return "test1";
     }
     
 }
